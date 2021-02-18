224ac4e60c899aacff5f103011036ebae6bf0e38
fabon <syobon.hinata.public@gmail.com>
2021-02-19 02:45:52 +0900

    Stop mapping ctrl to ESC

--- linux/.config/xkeysnail/config.py
+++ linux/.config/xkeysnail/config.py
@@ -5,7 +5,10 @@ from xkeysnail.transform import *
 
 define_timeout(0.4)
 
+define_modmap({
+    Key.CAPSLOCK: Key.LEFT_CTRL
+})
+
 define_multipurpose_modmap({
     Key.SPACE: [Key.SPACE, Key.LEFT_SHIFT],
-    Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]
 })
