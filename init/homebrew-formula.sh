658ca68a56ae00ae4779c91412629d9eef4de926
fabon <syobon.hinata.public@gmail.com>
2022-07-29 11:39:30 +0900

    Use poppler instead of xpdf

--- init/homebrew-formula.sh
+++ init/homebrew-formula.sh
@@ -5,7 +5,7 @@ set -eu
 FORMULAS=`brew list`
 ALREADY_INSTALLED=1
 
-for FORMULA in coreutils curl deno direnv exiftool fd ffmpeg findutils fswatch fzf fzy gawk git git-lfs gnu-sed grep hadolint imagemagick jq mecab node mupdf-tools pandoc pnpm rbenv ripgrep rlwrap rsync shellcheck skktools sl tig tldr tmux tree vim wget xpdf yt-dlp zsh
+for FORMULA in coreutils curl deno direnv exiftool fd ffmpeg findutils fswatch fzf fzy gawk git git-lfs gnu-sed grep hadolint imagemagick jq mecab node mupdf-tools pandoc pnpm poppler rbenv ripgrep rlwrap rsync shellcheck skktools sl tig tldr tmux tree vim wget yt-dlp zsh
 do
     if ! echo "$FORMULAS" | tr " " "\n" | grep "^$FORMULA$" > /dev/null; then
         echo "$FORMULA not found"
