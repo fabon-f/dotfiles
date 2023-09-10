d0bd8def7847fa32220a3be8e38d4dd5d33685f1
fabon <syobon.hinata.public@gmail.com>
2023-09-10 15:51:49 +0900

    Improve listing dotfiles

--- deploy.sh
+++ deploy.sh
@@ -17,16 +17,32 @@ is_ignored() {
     echo "$ignore" | grep -q " $1 "
 }
 
+ls_dotfiles() {
+    if command -v git > /dev/null; then
+        git ls-files dots | cut -c 6- | sed 's/\/.*//g'
+    else
+        find dots -maxdepth 1 -name ".*" | grep -v '.DS_Store' | cut -c 6-
+    fi
+}
+
+ls_binfiles() {
+    if command -v git > /dev/null; then
+        git ls-files bin | cut -c 5-
+    else
+        find bin -maxdepth 1 -mindepth 1 -not -name ".*" | cut -c 5-
+    fi
+}
+
 deploy() {
     cd $SCRIPT_DIR
-    find dots -maxdepth 1 -name ".*" | grep -v '.DS_Store' | cut -c 6- | while read file; do
+    ls_dotfiles | while read file; do
         is_ignored "$file" && echo "$file is ignored" && continue
         [ "$(readlink "$HOME/$file")" = "$SCRIPT_DIR/dots/$file" ] && continue;
         ln -sn$($FORCE && printf "f") "$SCRIPT_DIR/dots/$file" "$HOME/$file" || continue
     done
 
     mkdir -p "$HOME/bin"
-    find bin -maxdepth 1 -mindepth 1 -not -name ".*" | cut -c 5- | while read file; do
+    ls_binfiles | while read file; do
         is_ignored "$file" && echo "$file is ignored" && continue
         [ "$(readlink "$HOME/bin/$file")" = "$SCRIPT_DIR/bin/$file" ] && continue;
         ln -sn$($FORCE && printf "f") "$SCRIPT_DIR/bin/$file" "$HOME/bin/$file" || continue
